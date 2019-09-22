7c5f0c68a39a8b61063a06425f2004abbcfa6e93
Richard Robinson <rir@my.yorku.ca>
2019-09-21 21:42:50 -0400

    updated layout

--- calc.js
+++ calc.js
@@ -1,76 +1,43 @@
 let currentDisplayed = "",
-	operatorLastPressed = false,
-	equalsLastPressed = false;
+    operatorLastPressed = false,
+    equalsLastPressed = false;
 
-const displayResult = () => document.getElementById("calcDisplay").innerHTML = (currentDisplayed.toString().length >= 10
-	? parseFloat(currentDisplayed).toPrecision(9).replace("+", "")
-	: currentDisplayed
-).replace("(?<=\..*)0+$", "");
+const format = s => (s.toString().length >= 10 ? parseFloat(s).toPrecision(9).toString().replace("+", "") : currentDisplayed)
+	.toString()
+	.replace(/(?<=\..*)0+$/g, "");
 
 const toRadians = (angle) => angle * (Math.PI / 180);
 
-document.addEventListener('keypress', (e) => {
-	switch (e.key) {
-		case '1':
-		case '2':
-		case '3':
-		case '4':
-		case '5':
-		case '6':
-		case '7':
-		case '8':
-		case '9':
-		case '0':
-		case '.':
-			display(e.key);
-			break;
+document.addEventListener('keypress', e => document.getElementById(e.key).click());
 
-		case '+':
-		case '-':
-		case '*':
-		case '/':
-			onOperator(e.key);
-			break;
-
-		case "Enter":
-		case '=':
-			onEquals();
-			break;
-
-		case "c":
-			clear_s();
-			break;
-
-		default:
-			return;
-	}
-});
-
+/**
+ * Displays the current number or operator
+ * @param val the value to display
+ */
 function display(val) {
-	if (equalsLastPressed) currentDisplayed = "";
+    if (equalsLastPressed) currentDisplayed = "";
 
-	if (!operatorLastPressed && !equalsLastPressed && document.getElementById("calcDisplay").innerText.length >= 10) {
-		return;
-	}
+    if (!operatorLastPressed && !equalsLastPressed && document.getElementById("calcDisplay").innerText.length >= 10) {
+        return;
+    }
 
-	if (operatorLastPressed || equalsLastPressed) {
-		document.getElementById("calcDisplay").innerHTML = val;
-	}
-	else {
-		document.getElementById("calcDisplay").innerHTML += val;
-	}
+    if (operatorLastPressed || equalsLastPressed) {
+        document.getElementById("calcDisplay").innerHTML = val;
+    } else {
+        document.getElementById("calcDisplay").innerHTML += val;
+    }
 
-	currentDisplayed += val;
+    currentDisplayed += val;
 
-	operatorLastPressed = false;
-	equalsLastPressed = false;
+    operatorLastPressed = false;
+    equalsLastPressed = false;
 }
 
 function onOperator(op) {
-	if (operatorLastPressed) return;
+    if (operatorLastPressed) return;
 
     if (op === "sqrt") {
-        currentDisplayed = (s => s.toString())(Math.sqrt(toRadians(parseFloat(currentDisplayed))));
+        currentDisplayed = (s => s.toString())(Math.sqrt(parseFloat(currentDisplayed)));
     } else if (op === "sin") {
         currentDisplayed = (s => s.toString())(Math.sin(toRadians(parseFloat(currentDisplayed))));
     } else if (op === "cos") {
@@ -79,7 +46,8 @@ function onOperator(op) {
         currentDisplayed = (s => s.toString())(Math.tan(toRadians(parseFloat(currentDisplayed))));
     } else {
         //document.getElementById("calcDisplay").innerHTML = eval(currentDisplayed);
-		displayResult();
+        document.getElementById("calcDisplay").innerHTML = format(currentDisplayed);
+        displayResult();
         currentDisplayed += op;
 
         operatorLastPressed = true;
@@ -88,25 +56,25 @@ function onOperator(op) {
         return;
     }
 
-  //  document.getElementById("calcDisplay").innerHTML = currentDisplayed;
-	displayResult();
+    //  document.getElementById("calcDisplay").innerHTML = currentDisplayed;
+    document.getElementById("calcDisplay").innerHTML = format(currentDisplayed);
     equalsLastPressed = true;
 }
 
 function onEquals() {
-	if (operatorLastPressed) return;
+    if (operatorLastPressed) return;
 
     currentDisplayed = eval(currentDisplayed);
-    displayResult();
+    document.getElementById("calcDisplay").innerHTML = format(currentDisplayed);
 
-	equalsLastPressed = true;
-	operatorLastPressed = false;
+    equalsLastPressed = true;
+    operatorLastPressed = false;
 }
 
 function clear_s() {
-	equalsLastPressed = true;
-	operatorLastPressed = true;
-	currentDisplayed = "";
+    equalsLastPressed = true;
+    operatorLastPressed = true;
+    currentDisplayed = "";
 
-	document.getElementById("calcDisplay").innerHTML = "0";
+    document.getElementById("calcDisplay").innerHTML = "0";
 }
